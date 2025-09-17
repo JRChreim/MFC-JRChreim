30c14871ebe230ca0a4346e5d5f758a9ae0161d4
JRChreim <jrchreim@outlook.com>
2025-09-16 18:51:57 -0700

    Update xtcurvespvspt.m

--- misc/xtcurvespvspt.m
+++ misc/xtcurvespvspt.m
@@ -10,7 +10,7 @@ switch loc
         mfcPath = '/p/global/jrchreim/simulations/PhaseChange/1D/BubbleDynamics/StrongCollapse/6Eqn/';
         RelMod = {'pTFinalTest', 'pFinalTest'} ;
         % RelMod = {'pTFinalTest'} ;
-        DiscLevel = {'N160E3', 'N320E3', 'N640E3', 'N1280E3'} ;
+        DiscLevel = {'N1280E3'} ;
         % DiscLevel = {'N160E3'} ;
         compliment = 'Cartesian/BC-6/C000E-00';
         FigFolder = '/p/global/jrchreim/Figures/';
